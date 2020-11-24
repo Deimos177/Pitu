ab6836c538044c07358b9ec44ab1adb00cc7df77
Bruce Silva <14155@sky.com.br>
2020-11-23 23:02:02 -0300

    backend finished

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { Link } from '../models/link'
+import linksRepository from '../models/linksRepository';
 import linkRepository from '../models/linksRepository'
 
 function generateCode(){
@@ -20,23 +21,22 @@ async function postLink(req: Request, res: Response){
     res.status(201).json(link)
 }
 
-function getLink(req: Request, res: Response){
-    // const code = req.params.code as string
-    // const link = links.find(item => item.code === code)
-    // if(!link)
-    //     res.sendStatus(404)
-    // else
-    //     res.json(link)
+async function getLink(req: Request, res: Response){
+    const code = req.params.code as string
+    const link = await linkRepository.findByCode(code)
+    if(!link)
+        res.sendStatus(404)
+    else
+        res.json(link)
 }
 
-function hitLink(req: Request, res: Response){
-    // const code = req.params.code as string
-    // const index = links.findIndex(item => item.code === code)
-    // if(index === -1)
-    //     res.sendStatus(404)
-    // else
-    //     links[index].hits!++
-    //     res.json(links[index])
+async function hitLink(req: Request, res: Response){
+    const code = req.params.code as string
+    const link = await linksRepository.hit(code)
+    if(!link)
+        res.sendStatus(404)
+    else
+        res.json(link)
 }
 
 export default {
