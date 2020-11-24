d92e9fd3f83aaccaa86496869cfb03af28d837ab
Bruce Silva <14155@sky.com.br>
2020-11-24 19:09:56 -0300

    Fronted initiated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,6 @@ import database from './database'
 database.sync()
 console.log("Databse running at 3306")
 
-app.listen(3000)
+app.listen(3001)
 
-console.log('Server running at 3000')
+console.log('Server running at 3001')
